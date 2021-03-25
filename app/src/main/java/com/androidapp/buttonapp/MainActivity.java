e320e0fb1936658f089851e0a21b697f042ff747
Y-B Park <201921055@sangmyung.kr>
2021-03-25 13:43:23 +0900

    My first button app

---
+++ app/src/main/java/com/androidapp/buttonapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.androidapp.buttonapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
